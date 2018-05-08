dc5c00a232718875371a8fc520ebdc6427ea4101
abumus-ab <ridwan.binjesoh@gmail.com>
2018-05-09 00:52:01 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'eei4SHtJKvAcdh2dhOfFbodpHqSNOdTbS503jv3NtLvbzMdeVXRulWel0J5LJQZYsJeJbQs4obu/2iG5jUJksv8iMFdA05K7A9ILi7GSJUY/NyPXhd5R4yxJ/HtpzXv/Jkx0qkAy8NaIz/peKB7UFAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
